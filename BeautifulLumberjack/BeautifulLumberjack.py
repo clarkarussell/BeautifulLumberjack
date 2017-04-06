01d20e3b062b23bbdf903cfa3c40291b58d90220
clarkarussell <clarkarussell@gmail.com>
2017-04-05 21:19:48 -0600

    Removed extra user log file

--- BeautifulLumberjack/BeautifulLumberjack.py
+++ BeautifulLumberjack/BeautifulLumberjack.py
@@ -7,36 +7,55 @@ import io
 import picamera
 import cv2
 import numpy
+import time
 
-#Create a memory stream so the photos don't need to be saved in a file
-stream = io.BytesIO()
+#Record start time
+start_time = time.time()
 
-#Get the picture (low resolutio, so it should be fast)
-#Here you can also specify other parameters (like camera rotation)
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+#Define end time as 1 week from now (604,800 seconds)
+end_time = start_time + 60#4800
 
-#convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+#This loop lasts 1 minute.
+while time.time() < end_time:
 
-#Create an OpenCV image
-image = cv2.imdecode(buff, 1)
+    #Create a memory stream so the photos don't need to be saved in a file
+    stream = io.BytesIO()
 
-#Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('/home/pi/github/BeautifulLumberjack/haarcascade_frontalface_default.xml')
+    #Get the picture (low resolutio, so it should be fast)
+    #Here you can also specify other parameters (like camera rotation)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+        camera.capture(stream, format='jpeg')
 
-#Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    #convert the picture into a numpy array
+    buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 
-#Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    #Create an OpenCV image
+    image = cv2.imdecode(buff, 1)
 
-print("Found "+str(len(faces))+" face(s)")
+    #Load a cascade file for detecting faces
+    face_cascade = cv2.CascadeClassifier('/home/pi/github/BeautifulLumberjack/haarcascade_frontalface_default.xml')
 
-#Draw a rectange around every found face
-for(x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+    #Convert to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-#Save the result image
-    cv2.imwrite('result.jpg',image)
+    #Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    #Save a picture of the face if it's not too far away
+    if len(faces) >= 1 and faces[0][2] > 50:
+        print("The beautiful lumberjack sees "+str(len(faces))+" face(s)")      
+        #Draw a rectange around every found face
+        for(x,y,w,h) in faces:
+            cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+        #Save the result image with the date
+        pictime = time.asctime( time.localtime(time.time()))
+        cv2.imwrite('/home/pi/github/BeautifulLumberjack/faces/'+pictime+'.jpg', image)
+        #Append results to log file
+        with open("user_log.txt", "a") as log:
+            log.write("\n")
+            log.write("Found, "+str(len(faces))+", face at, "+pictime+", "+str(time.localtime(time.time())))
+
+localtime = time.asctime( time.localtime(time.time()))
+
+print "Program naturally finished at :", localtime
